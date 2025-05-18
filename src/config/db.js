157c0da5a8e623ab298004d4d9aec12f073932af
Harshit Gupta <114746121+Harshit-gupta-45107@users.noreply.github.com>
2025-05-18 11:42:03 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,20 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+import pg from 'pg';
+import 'dotenv/config';
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const { Pool } = pg;
+const pool = new Pool(
+  process.env.DATABASE_URL ? {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  } : {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  }
+);
 
-module.exports = pool;
+export default pool;
